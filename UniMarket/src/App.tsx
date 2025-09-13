0119bfda01442ef778569048e78b2152e21b99d8
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-09-13 08:24:48 +0200

    First commit, after installations

---
+++ UniMarket/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-amber-500 size-100'>
+        <Button> Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
